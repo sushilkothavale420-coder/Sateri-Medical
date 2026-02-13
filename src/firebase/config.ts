3ab0504103d287b7aad1fcb0b2f48812f640d857
sushil kothavale <sushil.kothavale420@gmail.com>
2026-02-13 08:37:38 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2090010656-9fb78",
+  "appId": "1:93618747059:web:7beee1101a7a8d452d62c6",
+  "apiKey": "AIzaSyBaZmHB7Ler4OgN_7DTAwXKoARm72OjuhI",
+  "authDomain": "studio-2090010656-9fb78.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "93618747059"
+};
