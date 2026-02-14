ecf988cc35c000994b5686f48bdb6fb2d55e6877
sushil kothavale <sushil.kothavale420@gmail.com>
2026-02-14 05:43:39 +0000

    i want app icon  that will appear in your browser tab "Sateri Medical" w

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <head>
         <title>Sateri Medical</title>
-        <link rel="icon" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><rect width='100' height='100' rx='10' fill='white'/><text x='50' y='55' text-anchor='middle' font-family='sans-serif' font-size='80' font-weight='bold' fill='%2316a34a'>S</text></svg>" />
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><rect width='100' height='100' fill='white'/><text x='50' y='40' text-anchor='middle' font-family='sans-serif' font-size='25' font-weight='bold' fill='%2316a34a'>Sateri</text><text x='50' y='75' text-anchor='middle' font-family='sans-serif' font-size='25' font-weight='bold' fill='%23f97316'>Medical</text></svg>" />
       </head>
       <body className="antialiased">
         <FirebaseClientProvider>
